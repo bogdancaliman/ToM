8070eb9a9114c9c74395a878f06d37f6a5ba4574
Rasinar Ioan <rasinarionut@yahoo.com>
2021-03-26 12:28:10 +0200

    Redirect index-auth based on session. Handling exceptions

--- project/src/main/java/com/project/project/controllers/AppController.java
+++ project/src/main/java/com/project/project/controllers/AppController.java
@@ -1,22 +1,36 @@
 package com.project.project.controllers;
 
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.project.project.services.SessionService;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class AppController {
+
+    @Autowired
+    private SessionService sessionService;
     
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, HttpServletRequest request) {
+        if(!sessionService.amILoggedIn(request))
+            return "redirect:/auth";
         model.addAttribute("name", "AuthPage");
         return "index";
     }
 
     @GetMapping("/auth")
-    public String auth() {
+    public String auth(Model model, HttpServletRequest request) {
+        if(sessionService.amILoggedIn(request))
+            return "redirect:/";
+
+        model.addAttribute("error", "");
+        model.addAttribute("user", "");
         return "auth";
     }
 
--- project/src/main/java/com/project/project/controllers/AuthController.java
+++ project/src/main/java/com/project/project/controllers/AuthController.java
@@ -2,8 +2,13 @@ package com.project.project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.project.project.exceptions.LogInException;
+import com.project.project.exceptions.PasswordMatchException;
+import com.project.project.exceptions.SystemException;
+import com.project.project.exceptions.UserNotFoundException;
 import com.project.project.models.Account;
 import com.project.project.services.AccountService;
 import com.project.project.services.SessionService;
@@ -19,17 +24,24 @@ public class AuthController {
     @Autowired
     private SessionService sessionService;
 
-    @PostMapping("/checkCredentials")
-    public String checkCredentials(@RequestParam("username") String username, @RequestParam("password") String password, HttpServletRequest request) {
-        if(!username.equals("") && !password.equals(""))
-        {
+    @PostMapping("/auth")
+    public String checkCredentials(@RequestParam("username") String username, @RequestParam("password") String password, Model model, HttpServletRequest request) {
+        model.addAttribute("user", username);
+        if (!username.equals("") && !password.equals("")) {
+            model.addAttribute("error", "");
             try {
                 Account acc = accountService.checkCredentials(username, password);
-                sessionService.addAccountToSession(acc, request);
-                return "index";
+                sessionService.addAccountSession(acc, request);
+                return "redirect:/";
+            } catch (UserNotFoundException e) {
+                model.addAttribute("error", "User not found!");
+            } catch (PasswordMatchException e) {
+                model.addAttribute("error", "Wrong password!");
+            } catch (SystemException | LogInException e) {
+                model.addAttribute("error", "We're having some system issues! Try again later!");
             }
-            catch (Exception e) {
-            }
+        } else {
+            model.addAttribute("error", "Please complete all fields!");
         }
         return "auth";
     }
