07fb32d446216e7f7627457ff4d54fc4d73812cf
Rasinar Ioan <rasinarionut@yahoo.com>
2021-04-14 09:40:30 +0300

    implemented and holidayRequest.Resolve the case where the form iploaded employees from another department

--- project/src/main/java/com/project/project/dtos/RequestType.java
+++ project/src/main/java/com/project/project/dtos/RequestType.java
@@ -1,8 +1,19 @@
 package com.project.project.dtos;
 
 public enum RequestType {
-    Med,
-    Rel,
-    Fam,
-    Hof
+    Med("Medical"),
+    Rel("Rest"),
+    Fam("Family situation"),
+    Hof("Home Office");
+
+
+    private final String displayValue;
+
+    private RequestType(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
--- project/src/main/java/com/project/project/repositories/AccountRepository.java
+++ project/src/main/java/com/project/project/repositories/AccountRepository.java
@@ -3,11 +3,16 @@ package com.project.project.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.project.project.models.Account;
+import com.project.project.models.Department;
+import com.project.project.models.Employee;
+
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Integer> {
     Account findByUsername(String username);
     Account findById(int id);
     Account findByEmployee_Id(int id);
-
+    List <Account> findAllByTl(Account tl);
+    List <Account> findAllByTlIsNullAndEmployee_Department(Department department);
 }
