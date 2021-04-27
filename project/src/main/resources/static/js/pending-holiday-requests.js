4742b1665cd4773fe64ca2700cd7d8f66617dfe4
Bogdan <bogdan.caliman112@gmail.com>
2021-04-27 10:08:19 +0300

    added background image. fixed a bug from holiday request page

--- project/src/main/resources/static/js/pending-holiday-requests.js
+++ project/src/main/resources/static/js/pending-holiday-requests.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
+	$('#requests').removeClass('invisible');
 
-    $("#requests").removeClass("invisible");
+	$('.requests__pending__entry').click(function () {
+		let id = $(this).attr('id');
+		$('#' + id + 'modal').modal({ show: true });
+	});
 
-    $(".requests__pending__entry").click(function () {
-        let id = $(this).attr("id");
-        $("#"+id+"modal").modal({show:true});
-    })
-
-    $(".update").click(
-        function () {
-            $.post("/tom/update-holiday-request", {
-                id: $(this).attr("id").replace('acc', '').replace('dec', ''),
-                act: $(this).attr("id").slice(0, 3)
-            }, function () {
-                document.location = '/pending-holiday-requests';
-            })
-        })
-}); 
+	$('.update').click(function () {
+		$.post(
+			'/tom/update-holiday-request',
+			{
+				id: $(this).attr('id').replace('acc', '').replace('dec', ''),
+				act: $(this).attr('id').slice(0, 3),
+			},
+			function () {
+				document.location = '/tom/pending-holiday-requests';
+			}
+		);
+	});
+});
